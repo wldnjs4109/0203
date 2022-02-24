cec5ca1b3a1a4a0201b9c8ba22c816c6949c7e64
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 15:32:34 +0900

    Create 2739.java

---
+++ 2739.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N;
+		Scanner s=new Scanner (System.in);
+		N=s.nextInt();
+ 
+		for(int i=1;i<=9;i++){
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+	}
+}
