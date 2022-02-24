08b25a80b7e90f7b6e1403c5a2aa8bb4abd4f8a2
wldnjs4109 <72204786+wldnjs4109@users.noreply.github.com>
2022-02-24 15:33:34 +0900

    Create 2742.java

---
+++ 2742.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N;
+		Scanner s = new Scanner(System.in);
+		N=s.nextInt();
+ 
+		for(int i=N;i>=1;i--) {
+			System.out.println(i);
+		}
+	}
+}
